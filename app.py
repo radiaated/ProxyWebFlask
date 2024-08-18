74c4194b714e37392470a29b5f355412925888a0
radiated <iamsumankht@gmail.com>
2024-08-18 15:54:04 +0545

    Initial commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+import requests
+import os
+
+app = Flask(__name__)
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+
+    url = os.environ.get("BASE_URL_FOR_PROXY") + "/" + path
+
+    r = requests.get(url).text
+
+    return r
+
+if __name__ == '__main__':
+    app.run()
